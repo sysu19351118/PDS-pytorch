17203678d6a395614912e410067cc40e4c227722
sysu19351118 <61107298+sysu19351118@users.noreply.github.com>
2024-01-26 18:49:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import os
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = '6'
 
-sys.argv.extend(['--cfg_file', 'configs/sbd_snake.yaml', 'demo_path', "/data/tzx/data/images", 'ct_score', '1.2','ff_num','0'])
+sys.argv.extend(['--cfg_file', 'configs/sbd_snake.yaml', 'demo_path', "/data/tzx/data/images", 'ct_score', '0.3','ff_num','0'])
 from run import run_demo
 run_demo() 
